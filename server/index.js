8961804946fc9ce1c0a438bcba6cc12dac989100
Kankina Tetiana <65289767+Klem26@users.noreply.github.com>
2023-02-13 19:56:32 +0200

    changed path

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ import dalleRouts from "./mongodb/routes/dalleRouts.js";
 dotenv.config();
 
 const app = express();
-// const cors = require("cors");
+
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
