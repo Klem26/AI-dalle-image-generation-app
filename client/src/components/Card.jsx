8afc9eefc04522d37410cbaed6d9caaaa1b82405
Kankina Tetiana <65289767+Klem26@users.noreply.github.com>
2023-02-14 20:59:42 +0200

    Added default image

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { download } from "../assets";
 import { downloadImg } from "../utils";
+import defaultImg from "../assets/defaultImg.jpg";
 
 const Card = ({ _id, name, prompt, photo }) => {
   return (
     <div className="rounded-xl group relative shadow-card hover:shadow-cardhover card">
       <img
         className="w-full h-auto object-cover rounded-xl"
-        src={photo}
+        src={_id === "63ea84937c36ddf10ef43491" ? defaultImg : photo}
         alt={prompt}
       />
       <div className="group-hover:flex flex-col max-h-[94.5%] hidden absolute bottom-0 left-0 right-0 bg-[#10131f] m-2 p-4 rounded-md">
