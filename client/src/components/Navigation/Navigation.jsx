a8f978378a82d45075894d443eea4a272d6884b1
Kankina Tetiana <65289767+Klem26@users.noreply.github.com>
2023-02-21 23:08:59 +0200

    added navigation

---
+++ client/src/components/Navigation/Navigation.jsx
@@ -0,0 +1,30 @@
+import { NavLink } from "react-router-dom";
+import { useAuth } from "../../hooks";
+
+import { logo, add } from "../../assets";
+
+export const Navigation = () => {
+  const { isLoggedIn } = useAuth();
+
+  return (
+    <nav
+      className="w-full flex gap-2
+    items-center bg-white   "
+    >
+      <NavLink to="/">
+        <img src={logo} alt="logo" className="w-28 object-contain" />
+      </NavLink>
+      <NavLink to="/create-post">
+        <img src={add} alt="logo" className="w-6 object-contain" />
+      </NavLink>
+      {isLoggedIn && (
+        <NavLink
+          to="/blog"
+          className="text-base font-medium text-gray-900 hover:text-gray-700"
+        >
+          Blog
+        </NavLink>
+      )}
+    </nav>
+  );
+};
